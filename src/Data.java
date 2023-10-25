a19954741d9b3d925325929f49d0ba163e4a6002
Adept <adeptiumion@gmail.com>
2023-10-25 13:57:34 +0700

    bb

--- src/Data.java
+++ src/Data.java
@@ -1,3 +1,19 @@
-public class Data {
+public class Data<T> {
+    private T hours;
+    private T minutes;
 
+    public Data(T hours, T minutes){
+        this.hours = hours;
+        this.minutes = minutes;
+    }
+
+    @Override
+    public String toString() {
+        return parser(hours) + ":" + parser(minutes);
+    }
+
+    private String parser(T time){
+        String buffer = time.toString();
+        return buffer.length() == 1 ? "0" + buffer : buffer;
+    }
 }
